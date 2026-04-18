eeafc2a34cb7f81f1d736e1d8e834fd516d8d0df
David Kosik <david.kosik@cez.cz>
2026-04-18 09:55:16 +0200

    feat: enhance site URL validation and streamline permission retrieval in Get-SPO-App-Permissions.ps1

--- Get-SPO-App-Permissions.ps1
+++ Get-SPO-App-Permissions.ps1
@@ -2,48 +2,59 @@
 # Set-SPO-App-Permissions
 #######################################################################################################################
 param(
-    [parameter(Mandatory = $true)][string]$Url,
-	[ValidateSet("read","write","fullcontrol","manage")]$Role="read",
-    [switch]$Delete,
-    [switch]$Force
+    [parameter(Mandatory = $true)][string]$Url
 )
 
 $ScriptName = $MyInvocation.MyCommand.Name
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 . $ScriptPath\include-Script-Start-Generic.ps1
-. $ScriptPath\include-Script-StdIncBlock.ps1
-. $ScriptPath\include-Functions-Common.ps1
+. $ScriptPath\include-Script-Start-Include.ps1
 
 #######################################################################################################################
 
 $SiteId = [string]::Empty
-
-Request-MSALToken -AppRegName $AppReg_SPO_MGMT -TTL 30
-Request-MSALToken -AppRegName $AppReg_LOG_READER -TTL 30
-
-#read all sites from SPO and check if site exists
-$UriResource = "sites"
-$UriSelect = "id,webUrl,name"
-$Uri = New-GraphUri -Version "v1.0" -Resource $UriResource -Select $UriSelect -Top 999
-[array]$Sites = Get-GraphOutputREST -Uri $Uri -AccessToken $AuthDB[$AppReg_SPO_MGMT].AccessToken -ContentType $ContentTypeJSON -Text "Getting SPO sites" -ProgressDots
-foreach ($Site in $Sites) {
-	if ($Site.webUrl -eq $Url) {
-        $SiteId = $Site.id
-        write-host "Site:        $Url ($($Site.name))" -ForegroundColor Yellow
-        write-host "Site id:     $SiteId" -ForegroundColor Gray
+$RelSiteUrl = $RootUrl = $null
+if ($Url.StartsWith("https://cezdata.sharepoint.com")) {
+    $RelSiteUrl = $Url.Replace("https://cezdata.sharepoint.com","")
+    $RootUrl = "cezdata.sharepoint.com"
+}
+else {
+    if ($Url.StartsWith("https://cezdata-my.sharepoint.com")) {
+        $RelSiteUrl = $Url.Replace("https://cezdata-my.sharepoint.com","")
+        $RootUrl = "cezdata-my.sharepoint.com"
     }
 }
-if ($SiteId -eq [string]::Empty) {
-	write-host "Site $Url not found" -ForegroundColor Red
-	Exit
+
+if (-not $RelSiteUrl) {
+    write-host "Url must start with https://cezdata.sharepoint.com or https://cezdata-my.sharepoint.com" -ForegroundColor Red
+    Exit
 }
+
+Request-MSALToken -AppRegName $AppReg_SPO_MGMT -TTL 30
+
+$UriResource = "sites/$($RootUrl):$RelSiteUrl"
+$Uri = New-GraphUri -Version "v1.0" -Resource $UriResource
+$Site = Get-GraphOutputREST -Uri $Uri -AccessToken $AuthDB[$AppReg_SPO_MGMT].AccessToken -ContentType $ContentTypeJSON
+
+if (-not $Site) {
+    write-host "Site not found: $Url" -ForegroundColor Red
+    Exit
+}
+
+$SiteId = $Site.id
+write-host "Site:        $Url ($($Site.name))" -ForegroundColor Yellow
+write-host "Site id:     $SiteId" -ForegroundColor Gray
+
 #existing permissions
 $UriResource = "sites/$SiteId/permissions"
 $Uri = New-GraphUri -Version "v1.0" -Resource $UriResource
 [array]$ExistingPermissions = Get-GraphOutputREST -Uri $Uri -AccessToken $AuthDB[$AppReg_SPO_MGMT].AccessToken -ContentType $ContentTypeJSON
 if ($ExistingPermissions.Count -gt 0) {
     foreach ($Permission in $ExistingPermissions) {
-        write-host "$($Permission.grantedToIdentitiesV2.application.id) $($Permission.grantedToIdentitiesV2.application.displayName)" -ForegroundColor DarkYellow
-        write-host $Permission.grantedToIdentities
+        write-host $string_divider
+        write-host "Roles: $($Permission.roles -join ", ")" -ForegroundColor Green
+        write-host "Application: $($Permission.grantedToIdentitiesV2.application.displayName)" -ForegroundColor Yellow
+        write-host "Application id: $($Permission.grantedToIdentitiesV2.application.id)"
     }
+    write-host $string_divider
 }
