7ab86b574559cb0244a73b9933298b9160d2f0b0
David Kosik <david.kosik@cez.cz>
2026-06-14 12:21:34 +0200

    feat: add source mailbox filtering and new scripts for migration progress and verification

---
+++ Get-T2T-Migration-Progress.ps1
@@ -0,0 +1,61 @@
+#######################################################################################################################
+# Get-T2T-Migration-Progress.ps1
+#######################################################################################################################
+param(
+    [Alias("Definitions","IniFile")][string]$VariableDefinitionFile
+)
+$ScriptName = $MyInvocation.MyCommand.Name
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+. $ScriptPath\include-Script-Start-Generic.ps1
+. $ScriptPath\include\include-functions-T2T.ps1
+
+#######################################################################################################################
+
+$LogFolder			= "t2t"
+$LogFilePrefix		= "t2t-migration-users"
+$LogFileFreq		= "Y"
+
+$OutputFolder = "t2t-ujvrez\migration-users"
+$OutputFilePrefix = "t2t-migration-users"
+
+#######################################################################################################################
+
+. $ScriptPath\include-Script-Start-Include.ps1
+
+$LogFile = New-OutputFile -RootFolder $RLF -Folder $LogFolder -Prefix $LogFilePrefix -Freq $LogFileFreq -Ext "log"
+$OutputFile = New-OutputFile -RootFolder $ROF -Folder $OutputFolder -Prefix $OutputFilePrefix -Freq "YMDHM" -Ext "csv"
+
+[array]$Report = @()
+
+$BatchPrefix = "UJV"
+$MigrationUsers = @()
+$TotalMigrationUsers = 0
+$TotalSyncedUsers = 0
+
+#######################################################################################################################
+
+$Dst_AppReg_EXO_MGMT = $AppReg_EXO_MGMT
+
+Connect-EXOService -AppRegName $Dst_AppReg_EXO_MGMT  -TTL 120
+
+$MigrationBatches = Get-MigrationBatch
+$MigrationBatches = $MigrationBatches | Where-Object { $_.Identity -like "$BatchPrefix*" }
+Write-host "Found $($MigrationBatches.Count) migration batches with prefix '$BatchPrefix'"
+
+foreach ($Batch in $MigrationBatches) {
+	write-host ($Batch.Identity.ToString()).PadRight(25) -ForegroundColor Green -NoNewline
+	$Result = Get-MigrationUser -BatchId $Batch.Identity
+	write-host ": $($Result.Count)"
+	$TotalMigrationUsers += $Result.Count
+	$SyncedUsers = $Result | Where-Object { $_.Status -eq "Synced" }
+	$TotalSyncedUsers += $SyncedUsers.Count
+	$MigrationUsers += $Result
+}
+
+write-host "Total migration users: $TotalMigrationUsers"
+write-host "Total synced users: $TotalSyncedUsers"
+write-host "Progress: $([math]::Round(($TotalSyncedUsers / $TotalMigrationUsers) * 100, 2)) %"
+
+#######################################################################################################################
+
+. $IncFile_StdLogEndBlock
